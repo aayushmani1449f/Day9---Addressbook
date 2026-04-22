041901fe0e702f898febf63f9b463e3f628376fd
Aayush Mani <aayushmani1449f@gmail.com>
2026-04-22 10:18:52 +0530

    UC4: Calculate Wages for a Month (20 Days)

--- src/com/employeepayroll/EmployeeWageMain.java
+++ src/com/employeepayroll/EmployeeWageMain.java
@@ -10,26 +10,31 @@ public class EmployeeWageMain {
         int FULL_DAY_HOURS = 8;
         int PART_TIME_HOURS = 8;
 
-        int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-        int empHrs = 0;
-        int dailyWage = 0;
+        int NUM_WORKING_DAYS = 20;
+        int totalWage = 0;
 
-        switch (empCheck) {
-            case 1: // IS_FULL_TIME
-                System.out.println("Employee is Present (Full-Time)");
-                empHrs = FULL_DAY_HOURS;
-                break;
-            case 2: // IS_PART_TIME
-                System.out.println("Employee is Present (Part-Time)");
-                empHrs = PART_TIME_HOURS;
-                break;
-            default:
-                System.out.println("Employee is Absent");
-                empHrs = 0;
-                break;
+        for (int day = 1; day <= NUM_WORKING_DAYS; day++) {
+            int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+            int empHrs = 0;
+            int dailyWage = 0;
+
+            switch (empCheck) {
+                case 1: // IS_FULL_TIME
+                    empHrs = FULL_DAY_HOURS;
+                    break;
+                case 2: // IS_PART_TIME
+                    empHrs = PART_TIME_HOURS;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
+            }
+
+            dailyWage = WAGE_PER_HOUR * empHrs;
+            totalWage += dailyWage;
+            System.out.println("Day " + day + " Wage: " + dailyWage);
         }
 
-        dailyWage = WAGE_PER_HOUR * empHrs;
-        System.out.println("Daily Wage: " + dailyWage);
+        System.out.println("Total Monthly Wage: " + totalWage);
     }
 }
