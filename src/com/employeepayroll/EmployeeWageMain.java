62fcfc34806bde58e1b99b5303c40ad4266f2103
Aayush Mani <aayushmani1449f@gmail.com>
2026-04-22 10:19:56 +0530

    UC7: Refactor the Code to write a Class Method to Compute Employee Wage

--- src/com/employeepayroll/EmployeeWageMain.java
+++ src/com/employeepayroll/EmployeeWageMain.java
@@ -1,18 +1,15 @@
 package com.employeepayroll;
 
 public class EmployeeWageMain {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Employee Wage Computation Program");
+    public static final int IS_FULL_TIME = 1;
+    public static final int IS_PART_TIME = 2;
+    public static final int WAGE_PER_HOUR = 20;
+    public static final int FULL_DAY_HOURS = 8;
+    public static final int PART_TIME_HOURS = 8;
+    public static final int MAX_WORKING_DAYS = 20;
+    public static final int MAX_WORKING_HOURS = 100;
 
-        int IS_FULL_TIME = 1;
-        int IS_PART_TIME = 2;
-        int WAGE_PER_HOUR = 20;
-        int FULL_DAY_HOURS = 8;
-        int PART_TIME_HOURS = 8;
-
-        int MAX_WORKING_DAYS = 20;
-        int MAX_WORKING_HOURS = 100;
-        
+    public static void computeWage() {
         int totalWage = 0;
         int totalDays = 0;
         int totalHours = 0;
@@ -24,10 +21,10 @@ public class EmployeeWageMain {
             int dailyWage = 0;
 
             switch (empCheck) {
-                case 1: // IS_FULL_TIME
+                case IS_FULL_TIME:
                     empHrs = FULL_DAY_HOURS;
                     break;
-                case 2: // IS_PART_TIME
+                case IS_PART_TIME:
                     empHrs = PART_TIME_HOURS;
                     break;
                 default:
@@ -36,9 +33,6 @@ public class EmployeeWageMain {
             }
 
             totalHours += empHrs;
-            // if adding empHrs exceeds max, we shouldn't necessarily cap it based on typical problem statement, or maybe we do. 
-            // Usually, it calculates for that day and stops if it reached/exceeded.
-
             dailyWage = WAGE_PER_HOUR * empHrs;
             totalWage += dailyWage;
             System.out.println("Day " + totalDays + " - Hours: " + empHrs + " - Wage: " + dailyWage);
@@ -46,5 +40,10 @@ public class EmployeeWageMain {
 
         System.out.println("Total Working Hours: " + totalHours);
         System.out.println("Total Monthly Wage: " + totalWage);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation Program");
+        computeWage();
     }
 }
