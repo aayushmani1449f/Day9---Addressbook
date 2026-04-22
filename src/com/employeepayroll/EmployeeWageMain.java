2a7935b315104f4d83d78fcbf984308a74d81358
Aayush Mani <aayushmani1449f@gmail.com>
2026-04-22 10:17:21 +0530

    UC1: Add attendance check and calculate daily wage

--- src/com/employeepayroll/EmployeeWageMain.java
+++ src/com/employeepayroll/EmployeeWageMain.java
@@ -3,5 +3,21 @@ package com.employeepayroll;
 public class EmployeeWageMain {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
+
+        int IS_FULL_TIME = 1;
+        int WAGE_PER_HOUR = 20;
+        int FULL_DAY_HOURS = 8;
+
+        int empCheck = (int) Math.floor(Math.random() * 10) % 2;
+        int dailyWage = 0;
+
+        if (empCheck == IS_FULL_TIME) {
+            System.out.println("Employee is Present");
+            dailyWage = WAGE_PER_HOUR * FULL_DAY_HOURS;
+        } else {
+            System.out.println("Employee is Absent");
+        }
+
+        System.out.println("Daily Wage: " + dailyWage);
     }
 }
